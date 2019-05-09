361cc8b62f189335fcabb4937c6bff33108a5e54
刘进行 <jackingstarliu@gmail.com>
2019-05-09 17:02:51 +0800

    Updates

---
+++ precache-manifest.3b0ec5862a1336f4606295ebb8aaa159.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3ece3d4ed091613d579e96bcad990c6",
+    "url": "/react-data-fetch-tutorial/index.html"
+  },
+  {
+    "revision": "cca198c0f7dccd254db0",
+    "url": "/react-data-fetch-tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "1d2f1bce547a7dadfc09",
+    "url": "/react-data-fetch-tutorial/static/js/2.7313cf0a.chunk.js"
+  },
+  {
+    "revision": "cca198c0f7dccd254db0",
+    "url": "/react-data-fetch-tutorial/static/js/main.2ded8e4e.chunk.js"
+  },
+  {
+    "revision": "fa2906fec918a0c655b3",
+    "url": "/react-data-fetch-tutorial/static/js/runtime~main.13a2b1f1.js"
+  }
+]);
